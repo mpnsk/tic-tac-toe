35de3819913c7d3fbc0b9024c354e4f64b78a5ff
mpnsk <mpnsk@users.noreply.github.com>
2018-02-05 21:58:56 +0100

    Add player switching game logic w/ tests

--- src/main/java/com/github/mpnsk/tic_tac_toe/Game.java
+++ src/main/java/com/github/mpnsk/tic_tac_toe/Game.java
@@ -1,32 +1,49 @@
 package com.github.mpnsk.tic_tac_toe;
 
+import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.HashSet;
 import java.util.Iterator;
+import java.util.List;
+import java.util.function.Predicate;
 
-public class Game {
+class Game {
 
-    private final String player1;
-    private final String player2;
     private String currentPlayer;
     private Iterator<String> players;
-    private final HashSet<String> playerPool;
+    private final List<String> playerPool;
+    private int[][] board;
 
-    public Game(String player1, String player2) {
-        this.player1 = player1;
-        this.player2 = player2;
-        playerPool = new HashSet<>(Arrays.asList(player1, player2));
+    Game(String... player) {
+        playerPool = new ArrayList<>(Arrays.asList(player));
         players = playerPool.iterator();
         endTurn();
+        board = new int[3][3];
     }
 
-    public String currentPlayer() {
+    String currentPlayer() {
         return currentPlayer;
     }
 
-    void markField(int x, int y) {
+    void markField(int x, int y) throws IllegalStateException, IllegalArgumentException {
+        testMoveForRange(x, y);
+        x--;
+        y--;
+        testMoveForAlreadyTaken(board[x][y]);
+        board[x][y] = playerPool.indexOf(currentPlayer);
 
         endTurn();
+    }
+
+    private void testMoveForAlreadyTaken(int playerNumber) {
+        if (playerNumber != 0)
+            throw new IllegalStateException("Impossible move, field already taken! Or badly initialized?");
+    }
+
+    private void testMoveForRange(int x, int y) {
+        int lower = 1, upper = 3;
+        Predicate<Integer> notInRange = integer -> integer < lower || integer > upper;
+        if (notInRange.test(x) || notInRange.test(y))
+            throw new IllegalArgumentException("Argument needs to be between " + lower + " and " + upper);
     }
 
     private void endTurn() {
--- src/test/java/com/github/mpnsk/tic_tac_toe/GameTest.java
+++ src/test/java/com/github/mpnsk/tic_tac_toe/GameTest.java
@@ -3,9 +3,10 @@ package com.github.mpnsk.tic_tac_toe;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 
 public class GameTest {
-    String player1 = "Adam", player2 = "Bob";
+    private String player1 = "Adam", player2 = "Bob";
 
     @Test
     public void currentPlayer() {
@@ -17,11 +18,30 @@ public class GameTest {
 
     @Test
     public void markField() {
+        Game game = new Game(player1, player2);
+        game.markField(1,1);
     }
 
     @Test
-    public void isWon() {
-        Game game = new Game("Adam", "Bob");
-        assertEquals(false, game.isWon());
+    public void game_started_and_not_won() {
+        Game game = new Game(player1, player2);
+        assertFalse(game.isWon());
+    }
+
+    @Test
+    public void game_played_and_won() {
+        Game game = new Game(player1, player2);
+        assertFalse(game.isWon());
+        game.markField(1,1);
+        assertFalse(game.isWon());
+        game.markField(2,1);
+        assertFalse(game.isWon());
+        game.markField(1,2);
+        assertFalse(game.isWon());
+        game.markField(2,2);
+        assertFalse(game.isWon());
+        game.markField(1,3);
+//        TODO check when game is actually won
+//        assertTrue(game.isWon());
     }
 }
